5f3efb2fa529f9b7c087e7863b0e952bc9dcf4d6
degraffbh <benjamindegraff@gmail.com>
2026-05-14 15:43:27 -0500

    Some more testing stuff

--- data/cart.js
+++ data/cart.js
@@ -1,16 +1,21 @@
-export let cart = JSON.parse(localStorage.getItem("cart"))
-if (!cart) {
-    cart = [{
-        productID: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-        quantity: 2,
-        deliveryOptionId: "1"
-    }, {
-        productID: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-        quantity: 1,
-        deliveryOptionId: "2"
-    }];
-}
+export let cart;
+loadFromStorage();
 let CART_TOTAL = 0;
+
+export function loadFromStorage() {
+    cart = JSON.parse(localStorage.getItem("cart"))
+    if (!cart) {
+        cart = [{
+            productID: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+            quantity: 2,
+            deliveryOptionId: "1"
+        }, {
+            productID: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+            quantity: 1,
+            deliveryOptionId: "2"
+        }];
+    }
+}
 
 function calculateCartQuantity() {
     CART_TOTAL = 0;
