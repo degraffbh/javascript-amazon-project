20d9854a49114d93b67716a70fe82591c17c9e5f
degraffbh <benjamindegraff@gmail.com>
2026-05-13 19:24:54 -0500

    Added jasmine testing framework

---
+++ tests/moneytest.js
@@ -0,0 +1,22 @@
+import formatCurrency from "../scripts/utils/money.js";
+
+console.log("converts cents into dollars");
+if (formatCurrency(2095) === "20.95") { //basic test case
+    console.log("test passed");
+} else {
+    console.log("test failed");
+}
+
+console.log("works with 0");
+if (formatCurrency(0) === "0.00") { //edge test case
+    console.log("test passed");
+} else {
+    console.log("test failed");
+}
+
+console.log("rounds up to the nearest cent");
+if (formatCurrency(2000.5) === "20.01") { //edge test case
+    console.log("test passed");
+} else {
+    console.log("test failed");
+}
