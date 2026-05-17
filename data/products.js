879ec633c8141f8a9ce195f4c0ad6918d71591f6
degraffbh <benjamindegraff@gmail.com>
2026-05-17 14:59:09 -0500

    Converts products into classes

--- data/products.js
+++ data/products.js
@@ -7,6 +7,26 @@ export function getProduct(productID) {
   return matchingProduct;
 }
 
+class Product {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+
+  constructor(productDetails) {
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+
+  getStarsURL() {
+    return `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+}
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -666,4 +686,6 @@ export const products = [
       "mens"
     ]
   }
-];
+].map((productDetails) => { //loops through every product and returns a class version of it
+  return new Product(productDetails);
+});
